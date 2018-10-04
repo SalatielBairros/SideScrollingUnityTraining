58ab1a7c3e963243dffc8fac2e7ed4a2f06ebfaf
SALATIEL COSTA BAIRROS <ALU201611865@esade.local>
2018-10-03 22:01:09 -0300

    Initial Commit

---
+++ Assets/Scripts/PlatformFall.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlatformFall : MonoBehaviour
+{
+  public float fallDelay = 1f;
+  private bool invokedFall = false;
+
+  private Rigidbody2D rb2d;
+
+  void Awake()
+  {
+    rb2d = GetComponent<Rigidbody2D>();
+  }
+
+  void OnCollisionEnter2D(Collision2D other)
+  {
+    if (other.gameObject.CompareTag("Player"))
+    {
+      Invoke("Fall", fallDelay);
+    }
+  }
+
+  void Fall()
+  {
+    rb2d.isKinematic = false;
+  }
+}
